91eaeac3fbf48b7e77dc67d2194a150e0337f565
SunethChinthaka <sunethedu@gmail.com>
2024-02-06 12:17:58 +0530

    Room and BookedRoom code added

--- src/main/java/com/suneth/hotel/model/BookedRoom.java
+++ src/main/java/com/suneth/hotel/model/BookedRoom.java
@@ -1,4 +1,74 @@
 package com.suneth.hotel.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookedRoom {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookingId;
+
+    @Column(name = "check_in")
+    private LocalDate checkInDate;
+
+    @Column(name = "check_out")
+    private LocalDate checkOutDate;
+
+    @Column(name = "guest_fullName")
+    private String guestFullName;
+
+    @Column(name = "guest_email")
+    private String guestEmail;
+
+    @Column(name = "adults")
+    private int numOfAdults;
+
+    @Column(name = "children")
+    private int numOfChildren;
+
+    @Column(name = "total_guests")
+    private int totalNumOfGuests;
+
+    @Column(name = "confirmation_code")
+    private String bookingConfirmationCode;
+
+    /**
+     * Represents the room booked.
+     * It's a Many-to-One relationship, indicating that many booked rooms can be associated with one room.
+     * It's fetched lazily, meaning it will be loaded from the database only when accessed.
+     */
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+    // Helper method to calculate total number of guests
+    public void calculateTotalNumberOfGuest() {
+        this.totalNumOfGuests = this.numOfAdults + numOfChildren;
+    }
+
+    // Setter method for the number of adults, triggers total guest calculation
+    public void setNumOfAdults(int numOfAdults) {
+        this.numOfAdults = numOfAdults;
+        calculateTotalNumberOfGuest();
+    }
+
+    // Setter method for the number of children, triggers total guest calculation
+    public void setNumOfChildren(int numOfChildren) {
+        this.numOfChildren = numOfChildren;
+        calculateTotalNumberOfGuest();
+    }
+
+//    public void setBookingConfirmationCode(String bookingConfirmationCode) {
+//        this.bookingConfirmationCode = bookingConfirmationCode;
+//    }
 }
--- src/main/java/com/suneth/hotel/model/Room.java
+++ src/main/java/com/suneth/hotel/model/Room.java
@@ -1,4 +1,63 @@
 package com.suneth.hotel.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.commons.lang3.RandomStringUtils;
+
+import java.math.BigDecimal;
+import java.sql.Blob;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
 public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String roomType;
+    private BigDecimal roomPrice;
+    private boolean isBooked = false;
+
+    @Lob
+    private Blob photo;
+
+    /**
+     * A list of BookedRoom entities associated with this room.
+     * It's a One-to-Many relationship, indicating that one room can have multiple bookings.
+     * It's fetched lazily, meaning it will be loaded from the database only when accessed.
+     * CascadeType.ALL: Indicates that if any operation is performed on the Room, the same operation should be cascaded
+     * to all its associated BookedRoom entities.
+     * For example, if a Room is deleted, all associated BookedRoom entities should also be deleted.
+     * This helps to maintain referential integrity between the entities.
+     */
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<BookedRoom> bookings;
+
+    public Room() {
+        this.bookings = new ArrayList<>();
+    }
+
+    /**
+     * Adds a booking to the room.
+     *
+     * @param booking The booked room to be added.
+     */
+    public void addBooking(BookedRoom booking) {
+        if (bookings == null) {
+            bookings = new ArrayList<>();
+        }
+        bookings.add(booking);
+        booking.setRoom(this);
+        isBooked = true;
+
+        // Generate a random booking confirmation code
+        String bookingCode = RandomStringUtils.randomNumeric(10);
+        booking.setBookingConfirmationCode(bookingCode);
+    }
 }
