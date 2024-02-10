555a6560c5dec17de38d54b67477a9d8946dd258
SunethChinthaka <sunethedu@gmail.com>
2024-02-10 09:25:40 +0530

    addNewRoom() created

--- src/main/java/com/suneth/hotel/repository/RoomRepository.java
+++ src/main/java/com/suneth/hotel/repository/RoomRepository.java
@@ -1,4 +1,7 @@
 package com.suneth.hotel.repository;
 
-public interface RoomRepository {
+import com.suneth.hotel.model.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomRepository extends JpaRepository<Room, Long> {
 }
--- src/main/java/com/suneth/hotel/service/RoomServiceImpl.java
+++ src/main/java/com/suneth/hotel/service/RoomServiceImpl.java
@@ -1,4 +1,32 @@
 package com.suneth.hotel.service;
 
-public class RoomServiceImpl {
+import com.suneth.hotel.model.Room;
+import com.suneth.hotel.repository.RoomRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.sql.rowset.serial.SerialBlob;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.Blob;
+import java.sql.SQLException;
+
+@Service
+@RequiredArgsConstructor
+public class RoomServiceImpl implements IRoomService {
+    private final RoomRepository roomRepository;
+
+    @Override
+    public Room addNewRoom(MultipartFile photo, String roomType, BigDecimal roomPrice) throws IOException, SQLException {
+        Room room = new Room();
+        room.setRoomType(roomType);
+        room.setRoomPrice(roomPrice);
+        if (!photo.isEmpty()) {
+            byte[] photoBytes = photo.getBytes();
+            Blob photoBlob = new SerialBlob(photoBytes);
+            room.setPhoto(photoBlob);
+        }
+        return roomRepository.save(room);
+    }
 }
