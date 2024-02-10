faac7fb99225d170ac535cbac157ce4e441b02d2
SunethChinthaka <sunethedu@gmail.com>
2024-02-10 17:04:40 +0530

    getAllRoomTypes() added

--- src/main/java/com/suneth/hotel/controller/RoomController.java
+++ src/main/java/com/suneth/hotel/controller/RoomController.java
@@ -5,19 +5,18 @@ import com.suneth.hotel.response.RoomResponse;
 import com.suneth.hotel.service.IRoomService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.sql.SQLException;
+import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/rooms")
+@RequestMapping("/api/v1/rooms")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173")
 public class RoomController {
     private final IRoomService roomService;
 
@@ -29,5 +28,9 @@ public class RoomController {
         Room savedRoom = roomService.addNewRoom(photo, roomType, roomPrice);
         RoomResponse response = new RoomResponse(savedRoom.getId(), savedRoom.getRoomType(), savedRoom.getRoomPrice());
         return ResponseEntity.ok(response);
+    }
+    @GetMapping("/room-types")
+    public List<String>getRoomTypes() {
+        return roomService.getAllRoomTypes();
     }
 }
